a25a7ccdec36e8bfbba9c5953cf04c17bc88ef6c
admin01-afk <akanmehmet125@gmail.com>
2025-03-07 22:00:34 +0300

    combined GetData function

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -10,84 +10,30 @@ namespace MauiApp1 {
     public partial class MainPage : ContentPage {
         public MainPage()
         {
-            //https://matse.eskisehir.edu.tr/tr/Duyuru
             InitializeComponent();
 
-            Task.Run(async () => {
-                GetMFData("https://mf.eskisehir.edu.tr/tr/Duyuru");
-            });
-            Task.Run(async () => {
-                GetMatseData("https://matse.eskisehir.edu.tr/tr/Duyuru");
-            });
+            FetchDataSequentially();
         }
 
-        async Task GetMFData(string url)
+        private async void FetchDataSequentially()
         {
-            MainThread.BeginInvokeOnMainThread(() => {
-                console.Text = "Fetching data...";
-                progressbar.IsVisible = true;
-                progressbar.Progress = 0.01;
-            });
-            try {
-                // FirefoxOptions for headless operation and optional image disabling for faster loading
-                FirefoxOptions options = new FirefoxOptions();
-                options.AddArgument("--headless");  // Optional: Run in headless mode
-                options.AddArgument("--disable-images");  // Optional: Disable images for faster loading
-
-                var driverService = FirefoxDriverService.CreateDefaultService();
-                driverService.HideCommandPromptWindow = true;
-
-                using IWebDriver driver = new FirefoxDriver(driverService,options);
-                driver.Navigate().GoToUrl(url);
-
-                // Wait for the page to load completely
-                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-
-                // Scroll the page to trigger more content loading (if necessary)
-                IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
-                js.ExecuteScript("window.scrollTo(0, document.body.scrollHeight/2);");
-                await Task.Delay(3000);  // Wait a bit for content to load after scrolling
-
-                // Wait for the element to load using the provided XPath
-                IWebElement parentDiv = wait.Until(d => d.FindElement(By.XPath("/html/body/div[5]/div/div[2]/div/div/div/div/div[2]/div/div/div[1]")));
-                IReadOnlyCollection<IWebElement> divsInsideParent = parentDiv.FindElements(By.XPath(".//div[contains(@class, 'gdlr-core-blog-full-frame gdlr-core-skin-e-background')]"));
-                int i = 0;
-                foreach (IWebElement div in divsInsideParent){//div[1]/div[2]/div[1]/div/h3/a
-                    string text = $"Duyuru{i}: " + div.FindElement(By.XPath("div[1]/div/h3/a")).Text + "\n";
-                    CreateElement(text,MF_list);
-                    i++;
-                    double progressValue = (double)i / 10;
-                    MainThread.BeginInvokeOnMainThread(() => {
-                        console.Text = $"Data-{i} Fetched";
-                        progressbar.ProgressTo(progressValue,200,Easing.Default);
-                    });
-                }
-                MainThread.BeginInvokeOnMainThread(() => {
-                    console.Text = "";
-                    progressbar.Progress = 0;
-                    progressbar.IsVisible = false;
-                });
-                driver.Quit();  // Ensure the driver quits after operation
-            } catch (Exception ex) {
-                MainThread.BeginInvokeOnMainThread(() => {
-                    console.Text = ex.Message;
-                });
-            }
+            await GetData("https://mf.eskisehir.edu.tr/tr/Duyuru", MF_list);
+            await GetData("https://matse.eskisehir.edu.tr/tr/Duyuru", Matse_list);
         }
 
-        async Task GetMatseData(string url)
+        async Task GetData(string url,VerticalStackLayout list)
         {
             MainThread.BeginInvokeOnMainThread(() => {
-                console.Text = "Fetching data...";
+                console.Text = $"Fetching {list.ClassId} data...";
                 progressbar.IsVisible = true;
                 progressbar.Progress = 0.01;
             });
             try {
                 FirefoxOptions options = new FirefoxOptions();
-                options.AddArgument("--headless");
+                options.AddArgument("--headless"); 
                 options.AddArgument("--disable-images");
 
-                FirefoxDriverService driverService = FirefoxDriverService.CreateDefaultService();
+                var driverService = FirefoxDriverService.CreateDefaultService();
                 driverService.HideCommandPromptWindow = true;
 
                 using IWebDriver driver = new FirefoxDriver(driverService, options);
@@ -95,18 +41,16 @@ namespace MauiApp1 {
 
                 WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
 
-                // Scroll the page to trigger more content loading (if necessary)
                 IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
                 js.ExecuteScript("window.scrollTo(0, document.body.scrollHeight/2);");
-                await Task.Delay(3000);  // Wait a bit for content to load after scrolling
+                await Task.Delay(2000);
 
-                // Wait for the element to load using the provided XPath
                 IWebElement parentDiv = wait.Until(d => d.FindElement(By.XPath("/html/body/div[5]/div/div[2]/div/div/div/div/div[2]/div/div/div[1]")));
                 IReadOnlyCollection<IWebElement> divsInsideParent = parentDiv.FindElements(By.XPath(".//div[contains(@class, 'gdlr-core-blog-full-frame gdlr-core-skin-e-background')]"));
                 int i = 0;
                 foreach (IWebElement div in divsInsideParent) {
                     string text = $"Duyuru{i}: " + div.FindElement(By.XPath("div[1]/div/h3/a")).Text + "\n";
-                    CreateElement(text, Matse_list);
+                    CreateElement(text, list);
                     i++;
                     double progressValue = (double)i / 10;
                     MainThread.BeginInvokeOnMainThread(() => {
@@ -119,18 +63,19 @@ namespace MauiApp1 {
                     progressbar.Progress = 0;
                     progressbar.IsVisible = false;
                 });
-                driver.Quit();  // Ensure the driver quits after operation
-            } catch (Exception e) {
+                driver.Quit();
+            } catch (Exception ex) {
                 MainThread.BeginInvokeOnMainThread(() => {
-                    console.Text = e.Message;
+                    console.Text = ex.Message;
                 });
             }
-
         }
+
         public HorizontalStackLayout CreateElement(string text,VerticalStackLayout list)
         {
-            HorizontalStackLayout element = new HorizontalStackLayout { 
-                Spacing=10,
+            HorizontalStackLayout element = new HorizontalStackLayout {
+                Spacing = 10,
+                ClassId = list.ClassId
             };
 
             Button button = new Button {
@@ -156,11 +101,17 @@ namespace MauiApp1 {
         }
         private void Button_Clicked(object sender, EventArgs e)
         {
-            Task.Run(async () => {
-                FirefoxOptions options = new FirefoxOptions();
-                using IWebDriver driver = new FirefoxDriver(options);
-                driver.Navigate().GoToUrl("https://mf.eskisehir.edu.tr/tr/Duyuru");
-            });
+            string url = "";
+            if(sender is Button btn) {
+                url = (btn.Parent.ClassId == "MF") ? "https://mf.eskisehir.edu.tr/tr/Duyuru" : "https://matse.eskisehir.edu.tr/tr/Duyuru";
+            }
+            OpenLink(url);
+        }
+        private async void OpenLink(string url)
+        {
+            if (await Launcher.CanOpenAsync(url)) {
+                await Launcher.OpenAsync(url);
+            }
         }
 
         private void Collapse_btn_Clicked(object sender, EventArgs e)
